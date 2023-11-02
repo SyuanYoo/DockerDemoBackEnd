da79bbbfe28dff1e8c51010d7ce9d4707d7703ba
SyuanYu <syuanyu@t3ex-group.com>
2023-11-02 16:23:09 +0800

    測試用範例

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using DockerDemoBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerDemoBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DOCKERDEMODBContext _context;
+
+        public UsersController(DOCKERDEMODBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] User login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == login.Username && u.Password == login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { message = $"Hi! {user.Username} 登入成功" });
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using DockerDemoBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+                      });
+});
 
 // Add services to the container.
 
@@ -6,6 +23,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DOCKERDEMODBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -15,6 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
